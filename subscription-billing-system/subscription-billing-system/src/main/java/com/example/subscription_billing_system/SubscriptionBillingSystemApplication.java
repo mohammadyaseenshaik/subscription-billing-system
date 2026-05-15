2f65d3ad73e6289880419ee6bc6a09fbb6920352
mohammadyaseenshaik <mohammadyaseen_shaik@srmap.edu.in>
2026-05-15 15:33:36 +0530

    initial commit

---
+++ subscription-billing-system/subscription-billing-system/src/main/java/com/example/subscription_billing_system/SubscriptionBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscription_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionBillingSystemApplication.class, args);
+	}
+
+}
